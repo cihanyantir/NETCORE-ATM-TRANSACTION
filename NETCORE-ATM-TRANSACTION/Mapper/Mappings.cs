019dd101f82c462b73780b5dff56d64dba49983a
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-02-16 20:40:15 +0300

    Mappings Configurations

---
+++ NETCORE-ATM-TRANSACTION/Mapper/Mappings.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NETCORE_ATM_TRANSACTION.Repository.Models;
+using NETCORE_ATM_TRANSACTION.Repository.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCORE_ATM_TRANSACTION.Mapper
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+            CreateMap<Account, AccountDTO>().ReverseMap();
+            CreateMap<Account, AccountCreateDTO>().ReverseMap();
+            CreateMap<Account, AccountUpdateDTO>().ReverseMap();
+            CreateMap<Customer, CustomerDTO>().ReverseMap();
+            CreateMap<Customer, CustomerUpdateDTO>().ReverseMap();
+            CreateMap<Customer, CustomerCreateDTO>().ReverseMap();
+        }
+    }
+}
