46bdaa3872418a9e5e0e19c6c1c3745d36996978
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-02-16 20:42:49 +0300

    CRUD Services

---
+++ NETCORE-ATM-TRANSACTION/Services/AccountCRUDService.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETCORE_ATM_TRANSACTION.IService;
+using NETCORE_ATM_TRANSACTION.Models;
+using NETCORE_ATM_TRANSACTION.Repository.Models;
+using SinKien.IBAN4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCORE_ATM_TRANSACTION.Services
+{
+    public class AccountCRUDService : IAccountCRUDService
+    {
+        
+        private readonly AtmDbContext _context ;
+
+        public AccountCRUDService(AtmDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool AccountExists(int id)
+        {
+            var values = _context.Accounts;
+            
+                return _context.Accounts.Any(x => x.AccountID == id);      }
+
+        public bool AccountExists(string IBAN)
+        {
+            bool value = _context.Accounts.Any(x => x.IBAN.ToLower().Trim() == IBAN.ToLower().Trim());
+            return value;
+        }
+
+        public void AddIBAN(Account account)
+        {
+            Iban iban = new IbanBuilder()
+                  .CountryCode(CountryCode.GetCountryCode("CZ"))
+                  .BankCode("0800")
+                  .AccountNumberPrefix("000019")
+                  .AccountNumber((account.AccountID).ToString())
+                  .Build();
+            account.IBAN = iban.ToString();
+            _context.SaveChanges();
+            
+        }
+
+        public bool CreateAccount(Account account)
+        {
+               
+            account.CreateDate = DateTime.UtcNow;
+            _context.Accounts.Add(account);
+                return Save();
+          
+        }
+
+
+
+        public bool DeleteAccount(Account account)
+        {
+          
+                _context.Accounts.Remove(account);
+                return Save();
+        
+        }
+
+        public Account GetAccount(int AccountID)
+        {
+           
+                return _context.Accounts.Include(x=>x.Customer).FirstOrDefault(x => x.AccountID == AccountID);
+         
+        }
+
+        public ICollection<Account> GetAccountByCustomer(int id)
+        {
+
+            //var values = _context.Accounts.Include(x => x.Customer).Where(x => x.AccountID == OriginAccountID).FirstOrDefault();
+            return _context.Accounts.Include(x => x.Customer).Where(x => x.Customer.CustomerID==id).ToList();
+        
+        }
+
+        public ICollection<Account> GetAccounts()
+        {
+          
+               return _context.Accounts.Include(x=>x.Customer).OrderBy(x => x.AccountID).ToList();           
+        }
+
+        public bool Save()
+        {
+                        return _context.SaveChanges() >= 0 ? true : false;
+           
+        }
+
+        public bool UpdateAccount(Account account)
+        {
+                        
+            _context.Accounts.Update(account);
+                return Save();
+
+            
+        }
+    }
+}
---
+++ NETCORE-ATM-TRANSACTION/Services/CustomerCRUDService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETCORE_ATM_TRANSACTION.IService;
+using NETCORE_ATM_TRANSACTION.Models;
+using NETCORE_ATM_TRANSACTION.Repository.Models;
+using SinKien.IBAN4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCORE_ATM_TRANSACTION.Services
+{
+    public class CustomerCRUDService :ICustomerCRUDService
+    {
+        private readonly AtmDbContext _context;
+
+        public CustomerCRUDService(AtmDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool CustomerExists(int id)
+        {
+            var values = _context.Customers;
+
+            return _context.Customers.Any(x => x.CustomerID == id);
+        }
+
+        
+
+        public bool CreateCustomer(Customer Customer)
+        {
+
+            Customer.MembershipDate = DateTime.UtcNow;
+            _context.Customers.Add(Customer);
+            return Save();
+
+        }
+
+        public bool DeleteCustomer(Customer Customer)
+        {
+
+            _context.Customers.Remove(Customer);
+            return Save();
+
+        }
+
+        public Customer GetCustomer(int CustomerID)
+        {
+
+            return _context.Customers.Include(x=>x.Accounts).FirstOrDefault(x => x.CustomerID == CustomerID);
+
+        }
+
+    
+        public ICollection<Customer> GetCustomers()
+        {
+
+            return _context.Customers.Include(x => x.Accounts).OrderBy(x => x.CustomerID).ToList();
+        }
+
+        public bool Save()
+        {
+            return _context.SaveChanges() >= 0 ? true : false;
+
+        }
+
+        public bool UpdateCustomer(Customer Customer)
+        {
+
+            _context.Customers.Update(Customer);
+            return Save();
+
+
+        }
+    }
+}
+
